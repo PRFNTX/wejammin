fa60672172eab55756d91f4b521e119843389910
Tarek Gabarin <tarekgabarin1992@gmail.com>
2022-06-28 23:32:10 -0400

    there was an attempt

--- ProjectGameJam/PlayerNode.gd
+++ ProjectGameJam/PlayerNode.gd
@@ -29,22 +29,11 @@ func kill_and_respawn():
 	get_parent().get_node("SpwawnLocation").spawn_player()
 	get_parent().get_node("SpawnLocation").spawn_player()
 	
-
-func play_player_gets_hits_animation():
-	# figure out why this shit don't work 
-	var current_animation_state = $AnimationTree.get("parameters/playback").get_current_node()
-	var is_idle = true if current_animation_state == "Idle" else false
-	$AnimationTree.get("parameters/playback").travel("Player_Hit")
-	var current_blend_position = $AnimationTree.get("parameters/Idle/blend_position")
-	print(current_blend_position)
-	$AnimationPlayer.set("parameters/Player_Hit/blend_position", current_blend_position)
-
 func change_health(new_health_value):
 	
 	if new_health_value == 0:
 		kill_and_respawn()
 	else:
-		play_player_gets_hits_animation()
 		current_health = new_health_value
 		$Healthbar.value = new_health_value
 		
@@ -96,7 +85,6 @@ func _physics_process(delta):
 		vertical_speed
 	).normalized()*player_speed
 	
-		
 	if initial_velocity == Vector2.ZERO:
 		$AnimationTree.get("parameters/playback").travel("Idle")
 	else:
